eec30e74c958eba35acd4a69e694545027882b0d
Chrisjohn Laxa <iamchrisjohnlaxa@gmail.com>
2024-02-10 20:16:13 +0800

    First commit

---
+++ public/js/dropdown.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-list').addEventListener('click', function() {
+    let mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
